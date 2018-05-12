24ca7e34e3fc0b9091d14be702cc56dc9b941c29
niall mcginness <njwmcginness@gmail.com>
2018-05-11 19:20:31 -0700

    added main terraform file main.tf

---
+++ main.tf
@@ -0,0 +1,25 @@
+
+provider "google" {
+  
+  project     = "<PROJECT-ID>"
+  region      = "us-west1"
+}
+
+resource "google_compute_instance" "default" {
+  count    =  1
+  name     =  "instance-${count.index}"
+  machine_type = "n1-standard-1"
+  zone         = "us-west1-a"
+
+ boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-8"
+    }
+  }
+
+metadata_startup_script = "echo test written in terraform script > /test.txt"
+
+  network_interface {
+    network = "default"
+  }
+}
